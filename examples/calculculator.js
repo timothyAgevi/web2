170f392faa642fedb10dfa9d6c1c18a528b3440b
timothyAgevi <timothyagevi@gmail.com>
2021-07-30 21:45:43 +0300

    solution

--- examples/calculculator.js
+++ examples/calculculator.js
@@ -2,5 +2,20 @@
 // divide and subtract any two numbers. -->
 
 
+document.write("This is my first program in Javascript<br>");
+var num1,num2,sub,mult,div,add;
+num1=prompt("Enter the first number: ");
+num2=prompt("Enter the second number: ");
+
+sub=num1-num2;
+add=num1+num2;
+div=num1/num2;
+mult=num1*num2;
+
+document.write("Sum = "+add+"<br>");
+document.write("Quotient = "+div+"<br>")
+document.write("Difference = "+sub+"<br>")
+document.write("Product = "+mult+"<br>")
+
     
     
