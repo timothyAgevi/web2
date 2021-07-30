51a2174d851ba04bdafe8326fadea1a0aa312680
timothyAgevi <timothyagevi@gmail.com>
2021-07-30 23:26:33 +0300

    solution to creditCard.js

--- examples/creditCard.js
+++ examples/creditCard.js
@@ -4,3 +4,12 @@
 // limit by spending $1,200.00 . If you do not buy anything else on 
 // the card and you do not make any payments, how much money would 
 // you owe the company after 6 months? 
+
+var r = 0.1249
+var n = 12
+var t = 0.5
+var p = 1200
+var A = p *Math.pow( 1+ r/n,n*t) 
+console.log(A)
+// line 13 in HTML would be as below:
+// document.write(`the amount is {A}`)
