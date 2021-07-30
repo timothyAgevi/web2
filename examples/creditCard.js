b17ae301ebce7ec6385f9592a1946a1c2830dbd3
timothyAgevi <timothyagevi@gmail.com>
2021-07-30 23:14:51 +0300

    instructions

---
+++ examples/creditCard.js
@@ -0,0 +1,6 @@
+// The first credit card that you got charges 12.49 % interest to 
+// its customers and compounds that interest monthly. Within one 
+// day of getting your first credit card, you max out the credit 
+// limit by spending $1,200.00 . If you do not buy anything else on 
+// the card and you do not make any payments, how much money would 
+// you owe the company after 6 months? 
