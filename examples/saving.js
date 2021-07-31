429844eefa264552cd8940ad0e0aefb1df3a2ed4
timothyAgevi <timothyagevi@gmail.com>
2021-07-31 12:25:36 +0300

    plan1

--- examples/saving.js
+++ examples/saving.js
@@ -2,3 +2,7 @@
 // want to invest all of the money in a savings account. 
 // However, your bank has two different plans. In 5 years from now, 
 // which plan will provide you with more money.
+
+// Plan 1: 	
+// The bank gives you a 6% interest rate and compounds the interest 
+// each month.
