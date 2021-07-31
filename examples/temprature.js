63d2208c948f2d2bbf8242ab64855aae57b57fc1
timothyAgevi <timothyagevi@gmail.com>
2021-07-31 13:22:21 +0300

    equivalent html below

--- examples/temprature.js
+++ examples/temprature.js
@@ -4,10 +4,34 @@
 // C=5/9(F-32)
 
 
-
+var f,c
 for(f=0;f <100;f+=10){
-    var f,c
+    
     c= 5/9 * (f-32)
     console.log( `FAHRENHEIT(F) \t Celcius`)
     console.log(f  ,  c )
 }
+
+// equivalent html synt
+{/* <html>
+<head>
+<title>Javascript</title>
+</head>
+
+<body>
+<script type="text/javascript">
+var f,c;
+document.write("Fahrenheit         Celcius<br>")
+for(f=0;f<=100;f=f+10)
+ {
+  c=5/9*(f-32);
+  document.write(f);
+  document.write("-------"+c+"<br>");
+
+ }
+
+</script>
+
+</body>
+
+</html> */}
