6cc5afaff6ee96c2a5b919bc937e0278b068e9a2
timothyAgevi <timothyagevi@gmail.com>
2021-07-31 13:15:36 +0300

    solution

--- examples/temprature.js
+++ examples/temprature.js
@@ -2,3 +2,12 @@
 // 0 AND 100 TO DEGREES CELCIUS USING THE FORMULA:
 
 // C=5/9(F-32)
+
+
+
+for(f=0;f <100;f+=10){
+    var f,c
+    c= 5/9 * (f-32)
+    console.log( `FAHRENHEIT(F) \t Celcius`)
+    console.log(f  ,  c )
+}
