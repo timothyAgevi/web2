3ef21851690b4abb60414368cca96a37eb1a229c
timothyAgevi <timothyagevi@gmail.com>
2021-07-31 12:24:36 +0300

    background info

---
+++ examples/saving.js
@@ -0,0 +1,4 @@
+// You win the lottery and get $1,000,000. You decide that you 
+// want to invest all of the money in a savings account. 
+// However, your bank has two different plans. In 5 years from now, 
+// which plan will provide you with more money.
